67e32dc579ededce9e5e94927c74811d1b9885e0
PyriRec <SamJacobsen@users.noreply.github.com>
2021-11-29 11:54:28 -0500

    Basic edge decompression

--- ds_types/dcx.py
+++ ds_types/dcx.py
@@ -6,15 +6,22 @@ from ds_types.binary_file import BinaryFile
 class DCX(BinaryFile):
 
     HEADER = b'DCX'
+    FIXED_HEADER_SIZE = 76
 
     def __init__(self, file):
         super(DCX, self).__init__(file)
+        self._header = self.file.read(DCX.FIXED_HEADER_SIZE)
 
-        magic = self.file.read(4)[:-1]
+        magic = self._header[0:3]
         if magic == DCX.HEADER:
+            self._name = [self.file.name.removesuffix('.dcx')]
+            self._format_method = self._header[40:44].decode('utf-8')
 
-            self.name = [self.file.name.removesuffix('.dcx')]
-            # todo: Read the full header, and check if the file is DEFLATE or EDGE
+            match self._format_method:
+                case 'EDGE':
+                    self._format = EDGE(self.file)
+                case 'DFLT':
+                    print('deflate format')
         else:
             raise Exception("Invalid header in dcx file")
 
@@ -29,3 +36,32 @@ class DCX(BinaryFile):
         # file_path: str = self.name.pop(0)
         # decompressed_data = zlib.decompress(self.file.read())
         # return file_path, decompressed_data
+
+
+class EDGE:
+
+    HEADER = b'EgdT'
+    FIXED_HEADER_SIZE = 36
+
+    def __init__(self, file):
+        self._file = file
+        self._header = self._file.read(EDGE.FIXED_HEADER_SIZE)
+
+        magic = self._header[0:4]
+        if magic == EDGE.HEADER:
+            self._edge_size = struct.unpack('>i', self._header[24:28])[0]
+            self._block_count = struct.unpack('>i', self._header[28:32])[0]
+
+            offset, size = self._read_struct_block()
+            self._file.seek(offset)
+            block = zlib.decompress(self._file.read(size), -zlib.MAX_WBITS)
+            print(block)
+        else:
+            raise Exception("Invalid header in edge")
+
+    def _read_struct_block(self):
+        self._file.read(4)
+        data_offset = struct.unpack('>i', self._file.read(4))[0] + self._edge_size + DCX.FIXED_HEADER_SIZE
+        data_size = struct.unpack('>i', self._file.read(4))[0]
+        self._file.read(4)
+        return data_offset, data_size
